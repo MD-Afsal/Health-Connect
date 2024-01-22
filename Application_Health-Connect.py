5add5c67568a98ac9011489662cf4c9d7e12f298
MD-Afsal <afsalce2023@gmail.com>
2024-01-22 16:40:29 +0530

    Updated in Lab

--- Application_Health-Connect.py
+++ Application_Health-Connect.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return render_template('Home_page.html')
+def home():
+    return render_template('head.html')
+
+
+@app.route('/Rotated_image')
+def rotated_image():
+    return render_template('Rotated_image.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
