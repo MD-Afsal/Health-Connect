6539090c49269b5fdaddfe7f0fad1a32e00b94b3
MD-Afsal <afsalce2023@gmail.com>
2024-01-29 22:23:57 +0530

    updated the connection with DB

--- Application_Health-Connect.py
+++ Application_Health-Connect.py
@@ -4,13 +4,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
+def login():
+    return render_template('Home_page.html')
+
+
+# first we need to redirect to the data upload page
+# but here we are navigating to the main page
+@app.route('/home')
 def home():
-    return render_template('head.html')
+    return render_template('Home_page.html')
 
 
 @app.route('/Rotated_image')
 def rotated_image():
     return render_template('Rotated_image.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ sample__01.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Admin93@"
+)
+#print("___-------____")
+print(mydb)
