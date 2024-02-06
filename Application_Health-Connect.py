7a651383e030cb791d788cfa6862a1340e1a3047
MD-Afsal <afsalce2023@gmail.com>
2024-02-06 16:48:07 +0530

    lab

--- Application_Health-Connect.py
+++ Application_Health-Connect.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import mysql.connector
+from flask import Flask, render_template,request,url_for,redirect,session,send_file,Response
+import mysql.connector,re
 
 app = Flask(__name__)
 
@@ -9,18 +9,39 @@ mydb = mysql.connector.connect(
     password="",
     database="HealthCon"
 )
-
-
 @app.route('/')
-def login():
-    return render_template('Rotated_image.html')
+@app.route('/login.html', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        user = request.form['iduser']
+        p0asw = request.form['idpass']
+        email = request.form['idemail']
+        cursor = mydb.connection.cursor(mydb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM tbl_signup WHERE username = % s', (user,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif len(user) < 4:
+            msg = 'User name must be greater than 3.'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', user):
+            msg = 'Username must contain only characters and numbers !'
+        else:
+            cursor.execute('INSERT INTO tbl_signup VALUES (NULL, % s, % s, % s)', (user, email, pasw))
+            mydb.connection.commit()
+            return render_template('index.html')
 
+    return render_template('login.html')
 
+@app.route('/signin')
+def signin():
+    return render_template('login.html')
 # first we need to redirect to the data upload page
 # but here we are navigating to the main page
-@app.route('/home')
-def home():
-    return render_template('Home_page.html')
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/Rotated_image')
---
+++ loginSample.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+@app.route('/')
+def index():
+    return 'Home Page'
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Please fill out all the fields', 'error')
+        elif User.query.filter_by(username=username).first():
+            flash('Username already exists. Please choose a different one.', 'error')
+        else:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created successfully. Please log in.', 'success')
+            return redirect(url_for('login'))
+
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username, password=password).first()
+
+        if user:
+            flash('Login successful', 'success')
+            # You can add code here to handle the logged-in user
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password', 'error')
+
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
