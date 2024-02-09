97c0a0b352a9e0e659200e0bcf5e12cfae7d5d55
MD-Afsal <afsalce2023@gmail.com>
2024-02-09 16:35:27 +0530

    Doctor Record Admin Page Added by Sri Akash

--- sample__01.py
+++ sample__01.py
@@ -3,9 +3,42 @@ from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="HealthCon"
+)
+mycursor = mydb.cursor()
+
+
 @app.route('/')
 def index():
     return render_template('adminPage.html')
+
+
+@app.route('/insert_rec', methods=['POST', 'GET'])
+def insert_rec():
+    if request.method == 'POST':
+        doc_name = request.form['doc_name']
+        category = request.form['doc_cat']
+        district = request.form['doc_dict']
+        city = request.form['doc_city']
+        address = request.form['doc_addr']
+        hospital_name = request.form['doc_hos']
+        phone = request.form['doc_num']
+        time_in = request.form['doc_time_in']
+        time_out = request.form['doc_time_out']
+
+        sql = '''insert into tbl_docter(doc_name, category, district, city, address, hospital_name, phone, 
+                time_IN, time_OUT) values(%s ,%s ,%s, %s ,%s ,%s, %s ,%s ,%s)'''
+
+        val = (doc_name, category, district, city, address, hospital_name, phone, time_in, time_out)
+
+        mycursor.execute(sql, val)
+        mydb.commit()
+        return render_template('adminPage.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
