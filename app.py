6806f4880c0e480132220e1fe133aedf22726aef
MD-Afsal <afsalce2023@gmail.com>
2024-02-10 23:14:14 +0530

    afsal 10-02-24

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
+    password="Admin93@",
     database="HealthCon"
 )
 mycursor = mydb.cursor()
@@ -40,6 +40,29 @@ def home():
 def rotateimg():
     return render_template('rotateimg.html')
 
+
+@app.route('/insert_rec', methods=['POST', 'GET'])
+def insert_rec():
+    if request.method == 'POST':
+        doc_name = request.form['doc_name']
+        category = request.form['doc_cat']
+        district = request.form['doc_dict']
+        city = request.form['doc_city']
+        address = request.form['doc_addr']
+        hospital_name = request.form['doc_hos']
+        phone = request.form['doc_num']
+        time_in = request.form['doc_time_in']
+        time_out = request.form['doc_time_out']
+
+        sql = '''insert into tbl_docter(doc_name, category, district, city, address, hospital_name, phone, 
+                time_IN, time_OUT) values(%s ,%s ,%s, %s ,%s ,%s, %s ,%s ,%s)'''
+
+        val = (doc_name, category, district, city, address, hospital_name, phone, time_in, time_out)
+
+        mycursor.execute(sql, val)
+        mydb.commit()
+        return render_template('adminPage.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
