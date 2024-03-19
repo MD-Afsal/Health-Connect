348531508d25fe07c999303fea39a5976a7a1bf0
MD-Afsal <afsalce2023@gmail.com>
2024-03-19 16:17:17 +0530

    updated

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ Session(app)
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Samsung753",
+    password="Admin93@",
     database="HealthCon"
 )
 mycursor = mydb.cursor()
@@ -23,6 +23,7 @@ def index():
     return render_template('main.html')
 
 
+
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
@@ -33,13 +34,7 @@ def signup():
         val = (user, pasw, email)
         mycursor.execute(sql, val)
         mydb.commit()
-        session['user'] = request.form.get('iduser')
-        session['password'] = request.form.get('idpass')
-        session['email'] = request.form.get('idemail')
-        mycursor.execute("select id from tbl_login where username=%s and password=%s",
-                         (session.get('user'), session.get('password')))
-        user_id = mycursor.fetchone()
-        session['id'] = user_id
+        session['user'] = request.form.get('user')
         return redirect(url_for('home'))
     return redirect(url_for('index'))
 
@@ -50,47 +45,20 @@ def signin():
         username = request.form['emailId']
         password = request.form['passwordId']
         mycursor.execute("SELECT * FROM tbl_login WHERE username=%s AND password=%s", (username, password))
-        user = mycursor.fetchall()
+        user = mycursor.fetchone()
         if user:
-            user1 = user[0]
-            session['id'] = user1[0]
-            session['user'] = user1[1]
-            session['email'] = user1[2]
-            session['password'] = user1[3]
-            # user refers the list with in a tuple and the user1 refers the 1st tuple on the list
-            return redirect(url_for('get_userinput'))
+            session['user'] = request.form.get('user')
+            return redirect(url_for('home'))
         else:
             msg = "Incorrect user or password!"
             return render_template('login.html', msg=msg)
     return redirect(url_for('index'))
 
 
-# getting the user details
-@app.route('/user_details', methods=['GET', 'POST'])
-def get_userinput():
-    if request.method == 'POST':
-        user_name = request.form['user_name']
-        image = request.files['user_img']
-        user_image = image.read()
-        user_age = request.form['user_age']
-        user_gender = request.form['user_gender']
-        user_bloodgrp = request.form['user_bloodgrp']
-        affected_with = request.form['user_affected_with']
-
-        sql = '''update tbl_user_details set name=%s, user_img=%s, age=%s, gender=%s,
-         blood_group=%s, affected_with=%s where id=%s'''
-
-        mycursor.execute(sql, (user_name, user_image, user_age, user_gender, user_bloodgrp,
-                               affected_with, session['id']))
-        mydb.commit()
-        return redirect(url_for('home'))
-    return render_template('user_details.html')
-
-
 # image retrival using id
 @app.route('/image/<int:image_id>')
 def get_image(image_id):
-    mycursor.execute("SELECT doc_img FROM tbl_doctor_details WHERE id = %s", (image_id,))
+    mycursor.execute("SELECT doc_img FROM tbl_docter WHERE id = %s", (image_id,))
     image_data = mycursor.fetchone()[0]
     # mycursor.close()
     return Response(image_data, mimetype='image/jpeg')  # Adjust mimetype based on your image type
@@ -101,18 +69,15 @@ def home():
     return render_template('main.html')
 
 
-@app.route('/home_')
-def home2():
-    return render_template('home2.html')
+@app.route('/backpose')
+def rotateimg():
+    return render_template('rotateimg.html')
 
 
-@app.route('/insert_doctor_rec', methods=['POST', 'GET'])
+@app.route('/insert_rec', methods=['POST', 'GET'])
 def insert_rec():
     if request.method == 'POST':
         doc_name = request.form['doc_name']
-        image = request.files['doc_img']
-        image_data = image.read()
-        image_path = f"uploads/{image.filename}"
         category = request.form['doc_cat']
         district = request.form['doc_dict']
         city = request.form['doc_city']
@@ -122,49 +87,15 @@ def insert_rec():
         time_in = request.form['doc_time_in']
         time_out = request.form['doc_time_out']
 
-        sql = '''insert into tbl_doctor_details(doc_name, doc_img, doc_img_path, category, district, city, address, 
-        hospital_name, phone, time_IN, time_OUT) values(%s ,%s ,%s, %s ,%s ,%s, %s ,%s, %s ,%s, %s)'''
+        sql = '''insert into tbl_docter(doc_name, category, district, city, address, hospital_name, phone, 
+                time_IN, time_OUT) values(%s ,%s ,%s, %s ,%s ,%s, %s ,%s ,%s)'''
 
-        val = (doc_name, image_data, image_path, category, district, city, address, hospital_name, phone, time_in, time_out)
+        val = (doc_name, category, district, city, address, hospital_name, phone, time_in, time_out)
 
         mycursor.execute(sql, val)
         mydb.commit()
     return render_template('adminPage.html')
-
-
-# for user input action (admin page)
-@app.route('/insert_user_rec', methods=['POST', 'GET'])
-def insert_user_rec():
-    global msg
-    if request.method == 'POST':
-        user_name = request.form['user_name']
-        user_pass = request.form['user_pass']
-        mycursor.execute("SELECT * FROM tbl_login WHERE username=%s AND password=%s", (user_name, user_pass))
-        user = mycursor.fetchone()
-        if user:
-            return render_template('userAccountModificationPage.html')    # create function for user modification in user profile
-        else:
-            msg = "Incorrect user or password!"
-    return render_template('adminPage.html', msg=msg)
-
-
-@app.route('/head')
-def head():
-    return render_template('head.html')
-
-
-@app.route('/get_doctor_details', methods=['GET'])
-def get_doctor_details():
-
-    category = request.args.get('category')
-
-    query = "SELECT * FROM tbl_doctor_details WHERE category = %s"
-    mycursor.execute(query, (category,))
-    doctor_details = mycursor.fetchall()
-
-    html_content = render_template('doctor_details.html', doctor_details=doctor_details)
-    return html_content
-
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
